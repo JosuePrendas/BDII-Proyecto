fba719ce6837faabefff3ea010e2f650a98bd5dd
Josue <josue.prendas8@gmail.com>
2020-06-19 14:38:12 -0600

    Configuration

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const Express = require("express");
 const Mongoose = require("mongoose");
 const BodyParser = require("body-parser");
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://bdproyecto-9d198.firebaseio.com",
+});
 
 var app = Express();
 
@@ -11,62 +18,72 @@ app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
 const PersonModel = Mongoose.model("person", {
-    nombre: String,
-    apellido1: String,
-    apellido2: String
+  nombre: String,
+  apellido1: String,
+  apellido2: String,
 });
 
 app.post("/person", async (request, response, next) => {
-    try {
-        var person = new PersonModel(request.body);
-        var result = await person.save();
-        response.send(result);
-    } catch (error) {
-        response.status(500).send(error);
-    }
+  try {
+    var person = new PersonModel(request.body);
+    var result = await person.save();
+    response.send(result);
+  } catch (error) {
+    response.status(500).send(error);
+  }
 });
 
 app.get("/people", async (request, response, next) => {
-    try {
-        var result = await PersonModel.find().exec();
-        response.send(result);
-    } catch (error) {
-        response.status(500).send(error);
-    }
+  try {
+    var result = await PersonModel.find().exec();
+    response.send(result);
+  } catch (error) {
+    response.status(500).send(error);
+  }
 });
 
 app.get("/person/:id", async (request, response, next) => {
-    try {
-        var person = await PersonModel.findById(request.params.id).exec();
-        response.send(person);
-    } catch (error) {
-        response.status(500).send(error);
-    }
+  try {
+    var person = await PersonModel.findById(request.params.id).exec();
+    response.send(person);
+  } catch (error) {
+    response.status(500).send(error);
+  }
 });
 
 app.put("/person/:id", async (request, response, next) => {
-    try {
-        var person = await PersonModel.findById(request.params.id).exec();
-        person.set(request.body);
-        var result = await person.save();
-        response.send(result);
-    } catch (error) {
-        response.status(500).send(error);
-    }
+  try {
+    var person = await PersonModel.findById(request.params.id).exec();
+    person.set(request.body);
+    var result = await person.save();
+    response.send(result);
+  } catch (error) {
+    response.status(500).send(error);
+  }
 });
 
 app.delete("/person/:id", async (request, response, next) => {
-    try {
-        var result = await PersonModel.deleteOne({ _id: request.params.id }).exec();
-        response.send(result);
-    } catch (error) {
-        response.status(500).send(error);
-    }
+  try {
+    var result = await PersonModel.deleteOne({ _id: request.params.id }).exec();
+    response.send(result);
+  } catch (error) {
+    response.status(500).send(error);
+  }
 });
 
 app.listen(3000, () => {
-    console.log("Escuchando desde el puerto 3000...");
+  console.log("Escuchando desde el puerto 3000...");
 });
 
+admin
+  .auth()
+  .verifyIdToken(idToken)
+  .then(function (decodedToken) {
+    let uid = decodedToken.uid;
+    // ...
+  })
+  .catch(function (error) {
+    // Handle error
+  });
 // npm install nodemon --save-dev para autocargar cada vez que se
 // hace un cambio
