21fd8bd6c32e261247f756b100522454ea20a1d4
Anthony Ulloa M <tonyum@estudiantec.cr>
2020-06-23 19:42:01 -0600

    Falta lo de mongoose

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const Express = require("express");
 const Mongoose = require("mongoose");
 const BodyParser = require("body-parser");
 var firebase = require("firebase/app");
-const cors = require('cors');
-const OAuth2Server = require('oauth2-server'); // instalen esto npm install oauth2-server
 
-/*
 var admin = require("firebase-admin");
 var serviceAccount = require("./serviceAccountKey.json");
 
@@ -13,7 +10,6 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://bdproyecto-9d198.firebaseio.com",
 });
-*/
 
 /*
 <!-- The core Firebase JS SDK is always required and must be listed first -->
@@ -44,23 +40,20 @@ var firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-//firebase.analytics();
+// firebase.analytics();
 
 var app = Express();
 
-//mongoose.Promise = global.Promise;
-var url = "mongodb://25.18.3.25:27017/people";
-Mongoose.connect(url, {
-  useNewUrlParser: true,
+var url = "mongodb://25.76.2.217:27017/productos";
+Mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true } )
+.then(() => {
+  console.log("Successfully connected to the database");
 })
-  .then(() => {
-    console.log("successfully connected to the database");
-  })
-  .catch((err) => {
-    console.log(`MongoDB connection error: ${err}`);
-    console.log("error connecting to the database");
-    process.exit();
-  });
+.catch((err) => {
+  console.log(`MongoDB connection error: ${err}`);
+  console.log("Error connecting to the database");
+  process.exit();
+});
 
 // Store the auth codes and access tokens in memory. In a real
 // auth server, you would store these in a database.
@@ -71,41 +64,15 @@ app.use(BodyParser.json());
 // Con bodyParser permitimos que pueda parsear JSON
 app.use(BodyParser.urlencoded({ extended: true }));
 
-/* const modelo = {
-  // We support returning promises.
-  getAccessToken: function() {
-    return new Promise('works!');
-  },
-
-  // Or, calling a Node-style callback.
-  getAuthorizationCode: function(done) {
-    done(null, 'works!');
-  },
-
-  // Or, using generators.
-  getClient: function*() {
-    yield somethingAsync();
-    return 'works!';
-  },
-
-  // Or, async/wait (using Babel).
-  getUser: async function() {
-    await somethingAsync();
-    return 'works!';
-  }
-};
-
-let oauth = new OAuth2Server({model: modelo}); */
-
-const PersonModel = Mongoose.model("person", {
-  nombre: String,
-  apellido1: String,
-  apellido2: String,
+const ProductoModel = Mongoose.model("/Producto", {
+  nombreProducto: String,
+  nombreProductor: String,
+  zona: String,
 });
 
-app.post("/person", async (request, response, next) => {
+app.post("/agregarProducto", async (request, response, next) => {
   try {
-    var person = new PersonModel(request.body);
+    var person = new ProductoModel(request.body);
     var result = await person.save();
     response.send(result);
   } catch (error) {
@@ -113,27 +80,29 @@ app.post("/person", async (request, response, next) => {
   }
 });
 
-app.get("/people", async (request, response, next) => {
+app.get("/listarProductos", async (request, response, next) => {
   try {
-    var result = await PersonModel.find().exec();
+    var result = await ProductoModel.find().exec();
     response.send(result);
   } catch (error) {
     response.status(500).send(error);
   }
 });
 
-app.get("/person/:id", async (request, response, next) => {
+app.get("/listarProductos/:lat/:long", async (request, response, next) => {
   try {
-    var person = await PersonModel.findById(request.params.id).exec();
-    response.send(person);
+    var lat = await ProductoModel.findById(request.params.lat).exec();
+    var long = await ProductoModel.findById(request.params.long).exec();
+    response.send(lat);
+    response.send(long);
   } catch (error) {
     response.status(500).send(error);
   }
 });
 
-app.put("/person/:id", async (request, response, next) => {
+app.put("/producto/:id", async (request, response, next) => {
   try {
-    var person = await PersonModel.findById(request.params.id).exec();
+    var person = await ProductoModel.findById(request.params.id).exec();
     person.set(request.body);
     var result = await person.save();
     response.send(result);
@@ -142,9 +111,9 @@ app.put("/person/:id", async (request, response, next) => {
   }
 });
 
-app.delete("/person/:id", async (request, response, next) => {
+app.delete("/producto/:id", async (request, response, next) => {
   try {
-    var result = await PersonModel.deleteOne({ _id: request.params.id }).exec();
+    var result = await ProductoModel.deleteOne({ _id: request.params.id }).exec();
     response.send(result);
   } catch (error) {
     response.status(500).send(error);
@@ -164,53 +133,27 @@ app.get("/login/:email/:password", async (request, response, next) => {
 
 firebase.auth().onAuthStateChanged(function(user){
   if(user){
-    console.log(user.uid);
-    console.log(user.email);
+    user.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+      console.log(user.uid);
+      console.log(user.email);
+      authenthication(idToken);
+    });
   }
 });
 
-// Generate an auth code and redirect to your app client's
-// domain with the auth code
-app.post('/code', (req, res) => {
-  // Generate a string of 10 random digits
-  const authCode = new Array(10).fill(null).map(() => Math.floor(Math.random() * 10)).join('');
-
-  authCodes.add(authCode);
-
-  // Normally this would be a `redirect_uri` parameter, but for
-  // this example it is hard coded.
-  res.redirect(`http://localhost:3000/oauth-callback.html?code=${authCode}`);
-});
-
-app.options('/token', cors(), (req, res) => res.end());
-app.options('/secure', cors(), (req, res) => res.end());
-
-// Verify an auth code and exchange it for an access token
-app.post('/token', cors(), (req, res) => {
-  if (authCodes.has(req.body.code)) {
-    // Generate a string of 50 random digits
-    const token = new Array(50).fill(null).map(() => Math.floor(Math.random() * 10)).join('');
-
-    authCodes.delete(req.body.code);
-    accessTokens.add(token);
-    res.json({ 'access_token': token, 'expires_in': 60 * 60 * 24 });
-  } else {
-    res.status(400).json({ message: 'Invalid auth token' });
-  }
-});
-
-// Endpoint secured by auth token
-app.get('/secure', cors(), (req, res) => {
-  const authorization = req.get('authorization');
-  if (!accessTokens.has(authorization)) {
-    return res.status(403).json({ message: 'Unauthorized' });
-  }
-  return res.json({ answer: 42 });
-});
+function authenthication(idToken){
+  // idToken comes from the client app
+  admin.auth().verifyIdToken(idToken).then(function(decodedToken) {
+  let uid = decodedToken.uid;
+  console.log(true);
+  return uid;
+}).catch(function(error) {
+    // Handle error
+    console.log(idToken);
+    return false;
+  });
+}
 
 app.listen(3000, () => {
   console.log("Escuchando desde el puerto 3000...");
 });
-
-// npm install nodemon --save-dev para autocargar cada vez que se
-// hace un cambio
